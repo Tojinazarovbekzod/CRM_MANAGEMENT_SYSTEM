b0b1dcca72fe8d9841d040ddde296526dcae7fc0
Tojinazarovbekzod <bekzodtojinazar@gmail.com>
2026-06-04 15:36:33 +0500

    Add jazzmin to INSTALLED_APPS and requirements.txt for enhanced admin interface

--- coffee_restaurant/settings.py
+++ coffee_restaurant/settings.py
@@ -26,6 +26,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(','
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
